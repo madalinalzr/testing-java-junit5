c189d55c570eadfd9d84b5612cd864f147b4471d
madalinalzr <72612476+madalinalzr@users.noreply.github.com>
2020-10-27 19:10:29 +0200

    use example from course

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -7,17 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class OwnerTest {
 
     @Test
-    void dependentAssertion(){
-        Owner owner = new Owner(1L, "Tom", "Jerry");
-        owner.setCity("Galatz");
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("Key West");
         owner.setTelephone("1231231234");
 
         assertAll("Properties Test",
                 () -> assertAll("Person Properties",
-                        () -> assertEquals("test", owner.getFirstName()),
-                        () -> assertEquals("test", owner.getLastName())),
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did not Match"),
+                        () -> assertEquals("Buck", owner.getLastName())),
                 () -> assertAll("Owner Properties",
-                        () -> assertEquals("test", owner.getCity()),
-                        () -> assertEquals("test", owner.getTelephone()));
+                        () -> assertEquals("Key West", owner.getCity(), "City Did Not Match"),
+                        () -> assertEquals("1231231234", owner.getTelephone())
+                ));
     }
 }
