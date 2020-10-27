3fcad99f8b7791331a236a782f9f76b1bf3b2368
madalinalzr <72612476+madalinalzr@users.noreply.github.com>
2020-10-27 20:13:14 +0200

    use display name annotation

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,17 +15,18 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Proper Name")
     @Test
     void index() {
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "wrong");
-        assertEquals("indexd", controller.index(), () -> "Another wrong message");
+        assertEquals("index", controller.index(), () -> "Another wrong message");
 
     }
 
     @Test
     void oupsHandler() {
-        assertTrue("asdf".equals(controller.oupsHandler()), () -> "This is some expensive" +
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive" +
                 "Message to build +" +
                 "for my test");
     }
