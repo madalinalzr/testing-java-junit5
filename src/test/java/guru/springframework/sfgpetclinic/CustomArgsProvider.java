04bf7fc63f30ba4ac4f03fabb773ff7cbc2cd5f2
Madalin Lazar <mada.lzr@gamil.com>
2020-10-30 18:05:59 +0200

    extensions

---
+++ src/test/java/guru/springframework/sfgpetclinic/CustomArgsProvider.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+import java.util.stream.Stream;
+
+public class CustomArgsProvider implements ArgumentsProvider {
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of("FL", 1, 1),
+                Arguments.of("OH", 1, 1),
+                Arguments.of("MI", 1, 1));
+    }
+}
--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,13 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import guru.springframework.sfgpetclinic.CustomArgsProvider;
 import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -65,4 +65,28 @@ class OwnerTest implements ModelTest {
     void csvFileInputTest(String stateName, int val1, int val2){
         System.out.println(stateName + " = " + val1 + " + " + val2);
     }
+
+    @DisplayName("method provider test")
+    @ParameterizedTest(name = "[{index}] {arguments} - {displayName}")
+    @MethodSource("getargs")
+    void methodProviderTest(String stateName, int val1, int val2){
+        System.out.println(stateName + " = " + val1 + " + " + val2);
+    }
+
+    static Stream<Arguments> getargs() {
+        return Stream.of(
+                Arguments.of("FL", 1, 1),
+                Arguments.of("OH", 1, 1),
+                Arguments.of("MI", 1, 1));
+
+    }
+
+    @DisplayName("class provider test")
+    @ParameterizedTest(name = "[{index}] {arguments} - {displayName}")
+    @ArgumentsSource(CustomArgsProvider.class)
+    void classProviderTest(String stateName, int val1, int val2){
+        System.out.println(stateName + " = " + val1 + " + " + val2);
+    }
+
+
 }
