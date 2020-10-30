8ea1fbdd701fa0349b3611649d31ce0ff78ad0b1
Madalin Lazar <mada.lzr@gamil.com>
2020-10-30 14:03:53 +0200

    csv source for tests

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -4,6 +4,9 @@ import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -32,9 +35,34 @@ class OwnerTest implements ModelTest {
     }
 
     @DisplayName("parameterized test")
-    @ParameterizedTest
+    @ParameterizedTest(name = "[{index}] {arguments} - {displayName}")
     @ValueSource(strings = {"ana", "are", "mere"})
     void testValueSource(String val){
         System.out.println(val);
+    }
+
+    @DisplayName("enum source test")
+    @ParameterizedTest(name = "[{index}] {arguments} - {displayName}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType){
+        System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV input source test")
+    @ParameterizedTest(name = "[{index}] {arguments} - {displayName}")
+    @CsvSource({
+            "FL, 1, 1",
+            "OH, 2, 2",
+            "MI, 1, 2"
+    })
+    void csvInputTest(String stateName, int val1, int val2){
+        System.out.println(stateName + " = " + val1 + " + " + val2);
+    }
+
+    @DisplayName("CSV file input source test")
+    @ParameterizedTest(name = "[{index}] {arguments} - {displayName}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFileInputTest(String stateName, int val1, int val2){
+        System.out.println(stateName + " = " + val1 + " + " + val2);
     }
 }
